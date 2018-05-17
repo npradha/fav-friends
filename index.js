b67bce12bdfe5adaf9286be14e4972d8bd19bc41
Netra Pradhan <npradha@purdue.edu>
2018-05-17 02:04:02 -0400

    Create individual functions to render item

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const form = document.querySelector('#epForm')
 const epArray = []
+
 function handleSubmit(ev){
     ev.preventDefault()
+
     const episodes = document.querySelector('#episodes')
     const ef = ev.target
     const favEp = ef.episode.value
-    const list = document.createElement('ul')
-   
-    list.appendChild(epItem(favEp))
-    episodes.appendChild(list)
+    
+    episodes.appendChild(epList(favEp))
     epArray.push(favEp)
 
     ef.reset()
@@ -16,40 +16,30 @@ function handleSubmit(ev){
 
 
 }
+function epList(episode){
+    const list = document.createElement('ul')
+    list.appendChild(epItem(episode))
+    return list
+}
 
 function epItem(episode){
-    const divItem = document.createElement('div')
-    
-   // divItem.style.width='fit-content'
-    //divItem.style.height='auto'
-    divItem.style.alignSelf='center'
-    //divItem.style.cssFloat='left'
-    // divItem.style.padding="10px"
-    
-    //divItem.style.borderStyle = 'none'
-    //divItem.styleborderColor='black'
-
     const item = document.createElement('li')
     item.style.listStyleType='none'
-    item.style.fontSize='20px'
-    item.textContent = episode
+    item.style.fontSize='30px'
+    item.textContent = `The One Where: ${episode}`
     item.style.textAlign = 'center'
-   
+    item.appendChild(createDelete())
+    return item
+}
 
+function createDelete(){
     const deleteButton = document.createElement('button')
+    deleteButton.style.marginLeft = '15px'
     deleteButton.textContent = 'Delete!'
     deleteButton.addEventListener('click',deleteItem)
-    
-    
-      // deleteButton.type='image';
-   // deleteButton.style.src='url(trash.png)'
-
-
-
-    item.appendChild(deleteButton)
-   divItem.appendChild(item)
-    return divItem
+    return deleteButton
 }
+
 function deleteItem(ev){
     ev.preventDefault();
     const epItem = ev.target
