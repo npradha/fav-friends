dd66dfe01d29e7afad72b41a92ae6d891c08e284
Netra Pradhan <npradha@purdue.edu>
2018-05-17 00:38:19 -0400

    Add delete button

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ function handleSubmit(ev){
     const ef = ev.target
     const favEp = ef.episode.value
     const list = document.createElement('ul')
-    /*
-    const item = document.createElement('li')
-    item.style.listStyleType = 'none'
-    item.style.fontSize = '20px'
-    item.textContent = favEp
-    */
+   
     list.appendChild(epItem(favEp))
     episodes.appendChild(list)
     epArray.push(favEp)
@@ -23,42 +18,31 @@ function handleSubmit(ev){
 }
 
 function epItem(episode){
-    /*const divItem = document.createElement('div')
-    divItem.style.border='50px solid transparent'
-    divItem.style.webkitBorderImage= 'url(frame.png) 30 30 stretch'
-    divItem.style.display='block'
-    divItem.style.width='50px'
-    divItem.style.height='100px'
-    divItem.style.padding="10px"
-    divItem.style.height='100px'
-    */
-   // divItem.style.borderStyle = 'dotted'
-   // divItem.styleborderColor='black'
-   /* const blankDiv = document.createElement('div')
-    blankDiv.style.position = 'relative'
-    const framePic = document.createElement('img')
-    framePic.src='frame.png'
-    framePic.style.position='relative'
-    framePic.style.width='20%'
-    framePic.style.height='30%'
-    //framePic.style.zIndex='-1'
-  */  
+    const divItem = document.createElement('div')
+    
+    divItem.style.width='fit-content'
+    divItem.style.height='auto'
+    divItem.style.alignSelf='center'
+    // divItem.style.padding="10px"
+    
+    divItem.style.borderStyle = 'dotted'
+    divItem.styleborderColor='black'
 
     const item = document.createElement('li')
-    //item.style.position='absolute'
-    //item.style.top = '0px'
-   // item.style.left='0'
-   // item.style.width='100%'
     item.style.listStyleType='none'
     item.style.fontSize='20px'
     item.textContent = episode
     item.style.textAlign='center'
-    item.style.wordWrap='break-word'
-   // item.style.zIndex='-1'
-    //framePic.appendChild(item)
-    //blankDiv.appendChild(framePic)
-    //blankDiv.appendChild(item);
-    return item
+   
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete!'
+    item.appendChild(deleteButton)
+    divItem.appendChild(item)
+
+
+
+    return divItem
 }
 
 
