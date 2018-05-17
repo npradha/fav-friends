1d4e3b002881e12611835053dc3f4fc8160dbf1c
Netra Pradhan <npradha@purdue.edu>
2018-05-16 20:05:03 -0400

    Create function to add item to list

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ function handleSubmit(ev){
     const ef = ev.target
     const favEp = ef.episode.value
     const list = document.createElement('ul')
+    /*
     const item = document.createElement('li')
     item.style.listStyleType = 'none'
     item.style.fontSize = '20px'
     item.textContent = favEp
-    list.appendChild(item)
+    */
+    list.appendChild(epItem(favEp))
     episodes.appendChild(list)
 
     ef.reset()
@@ -19,5 +21,12 @@ function handleSubmit(ev){
 
 }
 
+function epItem(episode){
+    const item = document.createElement('li')
+    item.style.listStyleType='none'
+    item.style.fontSize='20px'
+    item.textContent = episode
+    return item
+}
 
 form.addEventListener('submit',handleSubmit)
