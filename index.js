08a1e786b0a7062889c06489b671a6aac0f2a30c
Netra Pradhan <npradha@purdue.edu>
2018-05-16 21:16:02 -0400

    Add frame image to contain item

--- index.js
+++ index.js
@@ -23,16 +23,38 @@ function handleSubmit(ev){
 }
 
 function epItem(episode){
-    const divItem = document.createElement('div')
-    
-    divItem.style.borderStyle = 'dotted'
-    divItem.style.borderColor='black'
+    /*const divItem = document.createElement('div')
+    divItem.style.border='50px solid transparent'
+    divItem.style.webkitBorderImage= 'url(frame.png) 30 30 stretch'
+    divItem.style.display='block'
+    divItem.style.width='50px'
+    divItem.style.height='100px'
+    divItem.style.padding="10px"
+    divItem.style.height='100px'
+    */
+   // divItem.style.borderStyle = 'dotted'
+   // divItem.styleborderColor='black'
+    const blankDiv = document.createElement('div')
+    blankDiv.style.position = 'relative'
+    const framePic = document.createElement('img')
+    framePic.src='frame.png'
+    framePic.style.position='relative'
+    framePic.style.width='20%'
+    framePic.style.height='30%'
+    blankDiv.appendChild(framePic)
+
     const item = document.createElement('li')
+    item.style.position-'absolute'
+    //item.style.top = '0px'
+   // item.style.left='0'
+   // item.style.width='100%'
     item.style.listStyleType='none'
     item.style.fontSize='20px'
     item.textContent = episode
-    divItem.appendChild(item);
-    return divItem
+    item.style.textAlign='center'
+    item.style.wordWrap='break-word'
+    blankDiv.appendChild(item);
+    return blankDiv
 }
 
 
